f2c4b2e7183086538c008d8e9ee977ec2da4913d
Yi <alextoqiang@hotmail.com>
2024-03-17 11:51:34 -0400

    Update on 3/17
    Combined codes from Cong and Xiang.
    Generated output from new data

--- README.md
+++ README.md
@@ -8,6 +8,16 @@ This task include:
 3.	Summarizing and comparing RMSE and ME between RK and IWD in the interpolated maps.
 
 Outcomes:
--	[Interpolated maps by IDW](https://usf.app.box.com/folder/242904369567)
+-	[Interpolated maps by IDW using all data](https://usf.box.com/s/pumjyw0qz8qg323057h0o7w0ie8u9yy6)
+-	[Interpolated maps by IDW using only continuous data](https://usf.box.com/s/b99xo8qev4l48km9k8a971l4aqa04tbe)
 - [Interpolated maps by RK]()
-- [Performance metrics]()
+- [Comparison of ME, RMSE of the above three methods]()
+
+
+## Task 2b: IDW using Continuous Sites and Different Time Bins
+
+This task include:
+1.	Develop Python code to create interpolation maps using RK and IDW with barriers for 6 water quality parameters in 5 waterbodies in:
+  - **Seasons:** Run interpolations in combined seasons across years. For example, in GTM, combining samples in the falls in 2015 and 2016 to run the interpolation. This analysis will be done using both continuous data and discrete data, IDW and RK.
+  - **Monthly:** Use IDW to interpolate maps for six 30-day increments prior to the storm day, and then six 30-day increments following the storm day.
+  - **Weekly:** Use IDW to interpolate maps in 26 7-day increments prior to the storm day, and then 26 7-day increments following the storm day.
--- misc/idw_rk.py
+++ misc/idw_rk.py
@@ -5,8 +5,10 @@
 """
 import pandas as pd
 import numpy as np 
-import math,time,os
+import math,time,os,sys
 import arcpy
+
+arcpy.env.overwriteOutput = True
 
 
 # Predefined abbreviation of waterbody names and parameter names
@@ -51,38 +53,42 @@ def create_shp_season(df, output_folder):
     grouped = df.groupby(['WaterBody', 'Year', 'Season', 'Parameter'])
     
     for group_keys, group_df in grouped:
-        area, year, season, param = group_keys
-        
-        if area not in area_shortnames:
-            print(f"No managed area found with name: {area}")
+        try:
+            area, year, season, param = group_keys
+
+            if area not in area_shortnames:
+                print(f"No managed area found with name: {area}")
+                continue
+
+            if param not in param_shortnames:
+                print(f"No parameter found with name: {param}")
+                continue
+
+            group_df = group_df.dropna(subset=['x', 'y'])
+
+            if group_df.empty:
+                print(f"No valid data found for area: {area_shortnames[area]}, parameter: {param_shortnames[param]}, year: {year}, and season: {season}")
+                continue
+
+            print(f"Number of data rows for {area_shortnames[area]}, {param_shortnames[param]}, {year}, {season}: {len(group_df)}")
+
+            temp_csv_path = os.path.join(output_folder, f"temp_{area_shortnames[area]}_{param_shortnames[param]}_{year}_{season}.csv")
+            group_df.to_csv(temp_csv_path, index=False)
+
+            feature_class_name = f'SHP_{area_shortnames[area]}_{param_shortnames[param]}_{year}_{season}.shp'
+            feature_class_path = os.path.join(output_folder, feature_class_name)
+            spatial_reference = arcpy.SpatialReference(3086)
+
+            if arcpy.Exists(feature_class_path):
+                arcpy.Delete_management(feature_class_path)
+            arcpy.management.XYTableToPoint(temp_csv_path, feature_class_path, "x", "y", coordinate_system=spatial_reference)
+
+            os.remove(temp_csv_path)
+
+            print(f"Shapefile for {area_shortnames[area]}: {param_shortnames[param]} for year {year} and season {season} has been saved as {feature_class_name}") 
+        except Exception as e:
+            print(f"Error occurred: {e}")
             continue
-
-        if param not in param_shortnames:
-            print(f"No parameter found with name: {param}")
-            continue
-
-        group_df = group_df.dropna(subset=['x', 'y'])
-
-        if group_df.empty:
-            print(f"No valid data found for area: {area_shortnames[area]}, parameter: {param_shortnames[param]}, year: {year}, and season: {season}")
-            continue
-
-        print(f"Number of data rows for {area_shortnames[area]}, {param_shortnames[param]}, {year}, {season}: {len(group_df)}")
-
-        temp_csv_path = os.path.join(output_folder, f"temp_{area_shortnames[area]}_{param_shortnames[param]}_{year}_{season}.csv")
-        group_df.to_csv(temp_csv_path, index=False)
-
-        feature_class_name = f'SHP_{area_shortnames[area]}_{param_shortnames[param]}_{year}_{season}.shp'
-        feature_class_path = os.path.join(output_folder, feature_class_name)
-        spatial_reference = arcpy.SpatialReference(3086)
-
-        if arcpy.Exists(feature_class_path):
-            arcpy.Delete_management(feature_class_path)
-        arcpy.management.XYTableToPoint(temp_csv_path, feature_class_path, "x", "y", coordinate_system=spatial_reference)
-
-        os.remove(temp_csv_path)
-
-        print(f"Shapefile for {area_shortnames[area]}: {param_shortnames[param]} for year {year} and season {season} has been saved as {feature_class_name}")
    
                     
 # Function to fill NaN values in a specified column of a DataFrame with unique IDs, ensuring that each filled ID is unique.
@@ -230,8 +236,7 @@ def idw_interpolation(df, folder_path, output_folder, boundary_path, barrier_fol
                 arcpy.Delete_management(temp_dataset)
                 
             rmse, me = calculate_rmse_me(errors)
-            e_time =time.time() 
-            print(f"Calculated RMSE: {rmse}, ME: {me} for file: {filename}. Time used: {e_time - s_time}")
+
             print(f"File {filename} has completed {file_cross_validation_count} cross-validation iterations.")
             
             df.loc[df_update_index, 'RMSE'] = rmse
@@ -265,6 +270,8 @@ def idw_interpolation(df, folder_path, output_folder, boundary_path, barrier_fol
             # Save updates to csv
             df.to_csv(os.path.join(output_folder, "updated_idw.csv"), index=False)
             continue
+    e_time =time.time() 
+    print(f"Calculated RMSE: {rmse}, ME: {me} for file: {filename}. Time used: {e_time - s_time}")
              
     arcpy.ClearWorkspaceCache_management()
     arcpy.CheckInExtension("Spatial")
@@ -277,7 +284,86 @@ def idw_interpolation(df, folder_path, output_folder, boundary_path, barrier_fol
                 os.remove(file)
         except Exception as e:
             print(f"Error removing temp file {file}: {e}")          
-            
+
+# Function to interpolate water quality parameter values using RK method
+def rk_interpolation(method, radius, folder_path, waterbody, parameter, year, season, covariates, out_raster_folder,out_ga_folder,std_error_folder,diagnostic_folder):
+    area_shortnames = {
+        'Guana Tolomato Matanzas': 'GTM',
+        'Estero Bay': 'EB',
+        'Charlotte Harbor': 'CH',
+        'Biscayne Bay': 'BB',
+        'Big Bend Seagrasses':'BBS'
+    }
+    folder = os.path.join(folder_path+r"shapefiles")
+    shpName = []
+    for filename in os.listdir(folder):
+        if filename.endswith(".shp"):
+            shpName.append(filename)        
+    waterbody = waterbody
+    parameter = parameter
+    year      = str(year)
+    season    = season
+    covariates= str(covariates)
+    name1 = "SHP_" + "_".join([waterbody,parameter,year,season]) 
+    name  = name1 + ".shp"
+    name2 = "_".join([waterbody,parameter,year,season]) + "_RK"
+    
+    if name in shpName:
+        in_features = folder_path + "shapefiles/" + name
+        shapefile_path = os.path.join(folder_path+r"shapefiles", name)
+        data_count = int(arcpy.GetCount_management(shapefile_path).getOutput(0))
+        dependent_field = "ResultValu"
+        if "+" in covariates:
+            in_explanatory_rasters = []
+            covname_list = covariates.split("+")
+            for i in covname_list:
+                in_explanatory_raster = str(folder_path + "covariates/{}/{}.tif".format(i, waterbody))
+                in_explanatory_rasters.append(in_explanatory_raster)        
+        else:
+            in_explanatory_rasters = str(folder_path + "covariates/{}/{}.tif".format(covariates, waterbody))
+
+        out_ga_layer = out_ga_folder + name2 + "_ga"
+        out_raster   = out_raster_folder + name2 + ".tif"
+        out_std_error= std_error_folder + name2 + ".tif"
+        out_diagnostic_feature_class = diagnostic_folder + name2 + "_diag.shp"
+
+        mask = folder_path + "managed_area_boundary/"+  waterbody + ".shp"
+        smooth_r = radius
+        spatialref, c_size, parProFactor, extent = 3086, 30, "80%", arcpy.Describe(mask).extent
+        start_time = time.time()
+        try:
+            with arcpy.EnvManager(mask = mask,extent = extent,
+                                  outputCoordinateSystem = arcpy.SpatialReference(spatialref),
+                                  cellSize = c_size, 
+                                  parallelProcessingFactor = parProFactor):
+                out_surface_raster = arcpy.EBKRegressionPrediction_ga(in_features = in_features, 
+                                                                  dependent_field = dependent_field, 
+                                                                  out_ga_layer = out_ga_layer,
+                                                                  out_raster = out_raster,
+                                                                  in_explanatory_rasters = in_explanatory_rasters,
+                                                                  out_diagnostic_feature_class = out_diagnostic_feature_class,
+                                                                  transformation_type = 'EMPIRICAL',
+                                                                  search_neighborhood =arcpy.SearchNeighborhoodSmoothCircular(smooth_r,0.5))
+                arcpy.GALayerToRasters_ga(out_ga_layer, out_std_error,"PREDICTION_STANDARD_ERROR", None, c_size, 1, 1, "")
+                
+
+            with arcpy.da.SearchCursor(out_diagnostic_feature_class, ["RMSE","MeanError"]) as cursor:
+                data_points = [row for row in cursor]
+                rmse = data_points[0][0]
+                ME   = data_points[0][1]
+            print(f"Processing file: {name}")
+            print("--- Time lapse: %s seconds ---" % (time.time() - start_time))
+            return 1,rmse,ME,data_count,out_raster
+        except Exception:
+            e = sys.exc_info()[1]
+            print(parameter + " in " + str(year) + " " + season + " caused an error:")
+            print(e.args[0])
+            return 0,np.nan,np.nan,data_count,np.nan
+    else:
+        print(f"No data for RK interpolation in {name}, skipping")
+        return 0,np.nan,np.nan,0,np.nan
+
+# Function that calculate RMSE and ME    
 def calculate_rmse_me(errors):
     valid_errors = [e for e in errors if e is not None]
     if len(valid_errors) > 0: 
